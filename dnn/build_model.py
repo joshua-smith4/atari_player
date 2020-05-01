179c9fcdcb7906961c79315cde807787b5e613ef
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2020-04-30 21:20:12 -0600

    add qlearning agent and build_model with identical cnn as in paper

---
+++ dnn/build_model.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def build_model(input_shape, num_outputs):
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(16, (8, 8), padding="same", input_shape=input_shape, strides=4, activation='relu'))
+    model.add(layers.Conv2D(32, (4, 4), padding="same", strides=2, activation='relu'))
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dense(num_outputs, activation='linear'))
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
